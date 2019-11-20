3ed4b09c6ee6211b9cc763cf76b13f6691f10aad
Ozgur Can Arıcan <56658730+ozgurcanarican@users.noreply.github.com>
2019-11-20 23:39:49 +0300

    Add files via upload

---
+++ saygınTheOldMan.py
@@ -0,0 +1,221 @@
+import time
+print("Sevgili kardeşimiz Saygın. Bu mutlu gününde sana özel bir kod yazıp")
+print("Özgür biraderiminen birlikte sevincimizi, coşkumuzu ve biraz da seni")
+print("tanımış olmanın HAKLI pişmanlığını paylaşmak istedik.\n")
+print("Bu kod kapsamında cevaplaman için sana birkaç soru hazırladık.")
+print("yapman gereken çok basit, kodun sana sorduğu sorulara cevap vermek,")
+print("kodun verdiği çıktılara itiraz etmeden oyunun sonuna kadar")
+print("koddaki talimatları yerine getirerek oyunu tamamlamak.")
+print("Oyunun sonunda kodun sana bir çıktısı olacak. Umarım beğenirsin.")
+print("Biz eğlendik, sana da iyi eğlenceler diliyoruz...\n")
+print("Dipnot: lütfen cevaplarını sana sunulan listeden seç. ÜÇ NOKTA\n")
+print("************************************\n************************************\n")
+
+input("Oyuna başlamak için 'enter' a bas...\n")
+
+print("There we go !\n")
+
+print("NEREDE YAŞIYORSUN ?\n")
+print("hatay\nkarşıyaka\nbuca\nmezopotamya\n")
+
+time.sleep(1)
+while True:
+    cevap1 = input("cevabın :  ")
+    if cevap1 == "mezopotamya":
+        break
+    elif cevap1 == "hatay":
+        print("\nburada yaşadığına emin misin birader. bir daha düşün\n")
+    elif cevap1 == "buca":
+        print("\nburada yaşadığına emin misin birader. bir daha düşün\n")
+    elif cevap1 == "karşıyaka":
+        print("\nburada yaşadığına emin misin birader. bir daha düşün\n")
+    else :
+        print("\ndoğru yazıp yazmadığına, büyük küçük harflere dikkat et HIYARAĞASI")
+        
+
+print("\n\nEN SEVDİĞİN YEMEK ?\n")
+print("fasülye\npilav\ntaş kavurma\npizza\n")
+
+time.sleep(1)
+while True:
+    cevap2 = input("cevabın :  ")
+    if cevap2 == "taş kavurma":
+        break
+    elif cevap2 == "fasülye":
+        print("\nyeme bunu yeme, gaz yapar. Tıray Egeeğğnn\n")
+    elif cevap2 == "pilav":
+        print("\npilavdan dönenin kaşığı kırılsın ama cevabı bi gözden geçir\n")
+    elif cevap2 == "pizza":
+        print("\nekonomide de pizza yiyosun galiba. Tekrar dene.\n")
+    else :
+        print("\nAltı üstü yemek sorduk beceriksiz herif. Düzgün şekilde yaz. \nBoşluklara dikkat et\n")
+
+
+print("\n\nAYAKKABI NUMARAN?\n")
+print("40\n45\n50\n120\n")
+
+time.sleep(1)
+while True:
+    cevap3 = input("cevabın :  ")
+    if cevap3 == "120":
+        break
+    elif cevap3 == "40":
+        print("\nAyağı küçük olanın ... Bir daha ölç..\n")
+    elif cevap3 == "45":
+        print("\nAyağı küçük olanın ... Bir daha ölç..\n")
+    elif cevap3 == "50":
+        print("\nAyağı küçük olanın ... Bir daha ölç..\n")
+    else :
+        print("\nAyak numaranı sorduk yazdığın şeye bak.. tav etme adamı ciddi ol.\n")
+
+print("\n\nEN SEVDİĞİN ARKADAŞIN ?\n")
+print("melih\nözgür\nahmetcan\nbüyük iskender\n")
+
+time.sleep(1)
+while True:
+    cevap4 = input("cevabın :  ")
+    if cevap4 == "büyük iskender":
+        break
+    elif cevap4 == "melih":
+        print("\nSenin zamanında ben daha portakalda vitamindim. İyi düşün...\n")
+    elif cevap4 == "özgür":
+        print("\nben o kadar yaşlı değilim yha:// olmadı baştan...\n")
+    elif cevap4 == "ahmetcan":
+        print("\nVaayy biz varken ahmetcan he. peki.. al bunu al al al al..\n")
+    else :
+        print("\nİsim yazmayı bile beceremiyosun puh sana... Look at the liste, listeye...\n")
+
+print("\n\nFAVORİ AKTİVİTEN ?\n")
+print("spor\ntaş yontmak\nseyahat\nmüzik\n")
+
+time.sleep(1)
+while True:
+    cevap5 = input("cevabın :  ")
+    if cevap5 == "taş yontmak":
+        break
+    elif cevap5 == "spor":
+        print("\nBoksa gidiyo diye sporcu oldu hemen başımıza allahın hımbılı seni. Denemeye devam\n")
+    elif cevap5 == "seyahat":
+        print("\nBucadan dışarı çıkmayan adam mı söylüyo bunu. Güleyim bari :) TRY AGAINN \n")
+    elif cevap5 == "müzik":
+        print("\nDaha Melih'e diss atamıyon armut seni, dön başa bi daha dene\n")
+    else :
+        print("\nAktiviteyi listeden seç mabadından element uydurma\n")
+
+
+print("\n\nEN BÜYÜK DÜŞMANIN ?\n")
+print("greyfurt\nmuz\nerik\narmut\n")
+
+time.sleep(1)
+while True:
+    cevap6 = input("cevabın :  ")
+    if cevap6 == "greyfurt":
+        break
+    elif cevap6 == "muz":
+        print("\nAjdar'a özeniyosun ama yemezler koçum. Bi daha dene\n")
+    elif cevap6 == "erik":
+        print("\nAlalım 1 kilo erik,sana gelirken yerik sgvdsahd. Denemeye devam \n")
+    elif cevap6 == "armut":
+        print("\nmemnun oldum ben de Özgür ahahaha. Denemeye devam kabzımal seni.\n")
+    else :
+        print("\nkabzımallıkta erman toroğluyla yarışıyosun. hatta 1 adım öndesin.Listeden seç.\n")
+
+
+print("\n\nEN SIK KULLANDIĞIN KELİME ?\n")
+print("evet\nhayır\naga\nma'na-yı şi'r\n")
+
+time.sleep(1)
+while True:
+    cevap7 = input("cevabın :  ")
+    if cevap7 == "ma'na-yı şi'r":
+        break
+    elif cevap7 == "evet":
+        print("\nÖzgür ve ben de evet diyoruz ve seni 3 evetle uğurluyoruz. Return to beginning \n")
+    elif cevap7 == "hayır":
+        print("\nNayır nolamazz, fatih ölemezzz. Listeye iyi bak listeye. \n")
+    elif cevap7 == "aga":
+        print("\naga nigi naga nigiii. yahu bir cevap vercen ne uğraştırdın be kardeşim. Dene bi daha\n")
+    else :
+        print("\nZat-ı alinizin bahşetmiş olduğu cevab-ı sualler kodumuzu buga sokmakla vuku bulmuştur.")
+        print("Rica ediciim kodumuzu buga sokacak cevaplardan kaçınınız.Cevaplarınızı listede yazıldığı gibi yazınız.")
+        print("Tekrar deneyiniz.\n")
+
+
+print("\n\nNEREDEN MEZUNSUN ?\n")
+print("ilkokul\ndarü-l-fünun\nortaokul\nlise\n")
+
+time.sleep(1)
+while True:
+    cevap8 = input("cevabın :  ")
+    if cevap8 == "darü-l-fünun":
+        break
+    elif cevap8 == "ilkokul":
+        print("\nmedreseden başka okul mu gördün sanki. tekrar dene\n")
+    elif cevap8 == "ortaokul":
+        print("\nSaygın'ı rüştiyeye koymuşlar, ben askere gidecem demiş. olmadı paşam tekrar dene.\n")
+    elif cevap8 == "lise":
+        print("\nYou can not graduate from this high school. because you are in so deep. TRY AGAINN\n")
+    else :
+        print("\nmülkiyeler kapatıldı diye üzgünsün tamam ama oyunu bitirmen gerek az kaldı.\n Listedeki gibi yaz cevaplarını. Bir daha dene\n")
+
+
+print("\n\nKAÇ MEZUNUSUN ?\n")
+print("1857\n2010\n2011\n2012\n")
+
+time.sleep(1)
+while True:
+    cevap9 = input("cevabın :  ")
+    if cevap9 == "1857":
+        break
+    elif cevap9 == "2010":
+        print("\nBırak hadi yeme bizi. biraz ciddiyet be kardeşim. tekrar dene\n")
+    elif cevap9 == "2011":
+        print("\nküçül de cebime gir. TRY AGAIN\n")
+    elif cevap9 == "2012":
+        print("\nmaya takvimine göre hesapladın galiba. bir daha hesapla\n")
+    else :
+        print("\ndiploman olduğuna eminsin di mi koçum. 6 üstü yıl söyleyeceksin lan turunçgil. bi daha dene\n")
+
+print("\n\nSon sorumuza geldik. Bu soruda liste yok. Sadece cevabı girmen yeterli.")
+
+
+print("\n\nKAÇ YAŞINDASIN ?\n")
+
+while True:
+    try:
+        cevap10 = int(input("cevabın :  "))
+        if cevap10 >= 200:
+            break
+        else :
+            print("\n Rumi takvime göre değil miladi takvime göre cevap ver koçum. elini korkak alıştırma ver coşkuyu.")
+            print("O kadar küçük olmadığını biliyoruz. Çık çık uğraştırma bizi. yükselt biraz yaşını bak evde çoluk çocuk bekliyo")
+            print("Yaşınızı tekrar giriniz...\n")
+    except:
+        print("harf değil sayı gircen be bilader ne yaptın çökertcen programı. alksflkf")
+        print("yalnız bir şey dicem. Tekrar harf girersen program harbiden çöker. Baştan başlamak zorunda kalırsın.")
+        while True:
+            cevap10 = int(input("cevabın :  "))
+            if cevap10 >= 200:
+                break
+            else :
+                print("\n Rumi takvime göre değil miladi takvime göre cevap ver koçum. elini korkak alıştırma ver coşkuyu.")
+                print("O kadar küçük olmadığını biliyoruz. Çık çık uğraştırma bizi. yükselt biraz yaşını bak evde çoluk çocuk bekliyo")
+                print("Yaşınızı tekrar giriniz...\n")
+        break
+        
+
+
+print("\n\nCevapların işleniyor... Lütfen 10 sn bekleyiniz :):):):) ")
+time.sleep (10)
+
+print("\n\nSONUÇ:")
+time.sleep(2)
+print("\nDOĞUM GÜNÜN KUTLU OLSUN KARDEŞİM. BİRLİKTE NİCE MUTLU")
+print("YILLARIMIZ OLSUN İNŞALLAH. UMARIM OYUN BOYUNCA EĞLENMİŞSİNDİR")
+print("YENİ YAŞINDA HER ŞEY GÖNLÜNCE OLSUN. SEVİLİYOSUN...")
+print("\n\n\nCREATED BY ÖZGÜR UND MELİH dhfbdsjbdscdd :)))")
+
+input("\nçıkmak için Enter'a bas." )
+
+
+
